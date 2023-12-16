060c940f5a120ecbf65ca969fc024f2eb7a47b8b
UpenN <uruppu@gmail.com>
2023-12-16 08:50:50 -0500

    Checkin

---
+++ MatrixWalker.py
@@ -0,0 +1,16 @@
+"""Walk the Matrix problem"""
+class MatrixWalker:
+    def LefttotRight():
+        pass
+    def ToptoBottom():
+        pass
+    def RighttoLeft():
+        pass
+    def BottomtoTop():
+        pass
+
+    if __name__ == "__main__":
+        MatrixWalker()
+
+
+
